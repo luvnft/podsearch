d0f779b3fcf2697413374b2c1655aac6f7770019
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-05-25 22:10:27 +0200

    ok

--- poddley_client/components/SearchBoxSection/SearchBoxSection.vue
+++ poddley_client/components/SearchBoxSection/SearchBoxSection.vue
@@ -10,7 +10,8 @@
         type="text"
         name="search"
         id="search"
-        class="tw-block tw-w-full tw-rounded-none tw-rounded-l-md tw-border-gray-300 tw-pl-11 tw-text-left tw-text-base focus:tw-border-indigo-500 focus:tw-ring-indigo-500"
+        placeholder="Search for a quote in a podcast"
+        class="tw-block tw-w-full tw-justify-center tw-rounded-none tw-rounded-l-md tw-border-gray-300 tw-pl-11 tw-text-left tw-text-base focus:tw-border-indigo-500 focus:tw-ring-indigo-500"
       />
     </div>
     <button
@@ -19,7 +20,7 @@
       class="10 tw-group tw-flex tw-w-12 tw-items-center tw-justify-center tw-rounded-r-md tw-border-l tw-border-gray-300 tw-bg-gray-50 tw-p-2 tw-text-gray-700 tw-shadow-sm hover:tw-bg-gray-100 active:tw-shadow-none"
     >
       <IconsMagnifyingGlass class="tw-h-6 tw-w-6 tw-text-gray-400" v-if="!searchStore.loadingSearchResults" />
-      <LoadingIcon v-if="searchStore.loadingSearchResults" />
+      <IconsSpinnerIcon class="tw-h-6 tw-w-6 tw-text-gray-400" v-if="searchStore.loadingSearchResults" />
     </button>
   </div>
 </template>
