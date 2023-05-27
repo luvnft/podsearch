fb85dcfdda35758961b17c71ebeb2ed82af34fc4
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-05-28 01:29:38 +0200

    Ok

--- poddley_client/components/SearchEntry/SearchEntry.vue
+++ poddley_client/components/SearchEntry/SearchEntry.vue
@@ -2,10 +2,12 @@
   <div class="row tw-flex tw-flex-row tw-items-start tw-rounded-xl tw-border tw-border-white tw-bg-white tw-p-3 tw-shadow-md md:tw-gap-y-0">
     <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4 py-sm-2 tw-flex tw-items-start tw-justify-center tw-rounded-sm tw-py-3 tw-pb-5">
       <div class="tw-max-h-48 tw-w-80 tw-rounded-xl">
+        <img :src="props.searchEntry.imageUrl" alt="tailwind logo" class="tw-max-h-48 tw-rounded-xl tw-object-contain" v-if="!props.searchEntry.youtubeVideoLink" />
         <LiteYouTubeEmbed
           v-if="props.searchEntry.youtubeVideoLink"
           :id="(props.searchEntry.youtubeVideoLink.match(/v=([^&]+)/gi) || [''])[0].toString().slice(2)"
-          title="Rick Astley - Never Gonna Give You Up (Official Music Video)"
+          :title="props.searchEntry.episodeTitle"
+          :params="`start=${Math.floor(parseFloat(props.searchEntry.start.toString())) - Math.floor(parseFloat((props.searchEntry.deviationTime || 0).toString() || '0'))}`"
         />
       </div>
     </div>
