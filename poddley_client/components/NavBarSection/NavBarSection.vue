cc62e100a2f4ccbabbb1779ecd393728c38f1124
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-06-25 19:49:26 +0200

    More design fixes

--- poddley_client/components/NavBarSection/NavBarSection.vue
+++ poddley_client/components/NavBarSection/NavBarSection.vue
@@ -1,11 +1,11 @@
 <template>
-  <nav class="navbar navbar-light navbar-expand-md tw-pb-0 tw-shadow tw-shadow-slate-200 md:tw-pt-2 md:tw-pb-1 tw-pt-0">
-    <div class="container tw-max-w-screen-md tw-mb-0 tw-px-2.5 md:tw-px-5 tw-flex tw-justify-center">
+  <nav class="navbar navbar-light navbar-expand-md tw-pb-0 tw-pt-0 tw-shadow tw-shadow-slate-200 md:tw-pb-1 md:tw-pt-2">
+    <div class="container tw-mb-0 tw-flex tw-max-w-screen-md tw-justify-center tw-px-2.5 md:tw-px-5">
       <div class="row g-0 w-100">
-        <div class="col-4 col-md-6 tw-flex align-items-center justify-content-center tw-p-2 md:tw-justify-start">
+        <div class="col-4 col-md-6 align-items-center justify-content-center tw-flex tw-p-2 md:tw-justify-start">
           <NavTitle />
         </div>
-        <div class="col-8 col-md-6 tw-flex align-items-center tw-justify-center tw-gap-1.5">
+        <div class="col-8 col-md-6 align-items-center tw-flex tw-justify-center tw-gap-1.5">
           <SearchBoxSection />
         </div>
       </div>
