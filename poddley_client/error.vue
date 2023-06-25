835408cb61c8e8bd04126b85471428845ea5c0a4
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-06-25 22:01:37 +0200

    route fix possibly?

--- poddley_client/error.vue
+++ poddley_client/error.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tw-min-w-screen tw-flex tw-min-h-screen tw-items-center tw-justify-center">
-    <NuxtLink to="/new">
+    <NuxtLink to="new">
       (404) Error occurred. Click here to go back
     </NuxtLink>
   </div>
