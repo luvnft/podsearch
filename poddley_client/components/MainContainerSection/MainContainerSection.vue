1cb5e8fb25ce3ffe5271b2bfbd013ab244d36cc8
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-06-25 02:00:58 +0200

    Fixes

--- poddley_client/components/MainContainerSection/MainContainerSection.vue
+++ poddley_client/components/MainContainerSection/MainContainerSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tw-bg-slate-50 tw-min-h-screen tw-p-7 md:tw-p-7">
+  <div class="tw-bg-slate-50 tw-min-h-screen tw-pr-1 tw-pl-1 tw-pb-1 tw-pt-7 md:tw-p-7">
     <div class="container tw-max-w-screen-md">
       <slot />
     </div>
--- poddley_client/components/NavBarSection/NavBarSection.vue
+++ poddley_client/components/NavBarSection/NavBarSection.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="navbar navbar-light navbar-expand-md tw-py-0 tw-shadow tw-shadow-slate-200 md:tw-pt-2 md:tw-pb-1">
-    <div class="container tw-max-w-screen-md mb-1 tw-px-2.5 md:tw-px-5 tw-flex tw-justify-center">
+  <nav class="navbar navbar-light navbar-expand-md tw-pb-0 tw-shadow tw-shadow-slate-200 md:tw-pt-2 md:tw-pb-1 tw-">
+    <div class="container tw-max-w-screen-md tw-mb-0 tw-px-2.5 md:tw-px-5 tw-flex tw-justify-center">
       <div class="row g-0 w-100">
         <div class="col-6 tw-flex align-items-center justify-content-center tw-p-2 md:tw-justify-start">
           <NavTitle />
