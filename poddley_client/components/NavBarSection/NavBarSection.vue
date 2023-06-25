be65a5005db61ac423c8a887ed4b4c87a967b64c
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-06-25 19:47:49 +0200

    Design fixes

--- poddley_client/components/NavBarSection/NavBarSection.vue
+++ poddley_client/components/NavBarSection/NavBarSection.vue
@@ -1,11 +1,11 @@
 <template>
-  <nav class="navbar navbar-light navbar-expand-md tw-pb-0 tw-shadow tw-shadow-slate-200 md:tw-pt-2 md:tw-pb-1 tw-">
+  <nav class="navbar navbar-light navbar-expand-md tw-pb-0 tw-shadow tw-shadow-slate-200 md:tw-pt-2 md:tw-pb-1 tw-pt-0">
     <div class="container tw-max-w-screen-md tw-mb-0 tw-px-2.5 md:tw-px-5 tw-flex tw-justify-center">
       <div class="row g-0 w-100">
-        <div class="col-6 tw-flex align-items-center justify-content-center tw-p-2 md:tw-justify-start">
+        <div class="col-4 col-md-6 tw-flex align-items-center justify-content-center tw-p-2 md:tw-justify-start">
           <NavTitle />
         </div>
-        <div class="col-6 tw-flex align-items-center tw-justify-center tw-gap-1.5">
+        <div class="col-8 col-md-6 tw-flex align-items-center tw-justify-center tw-gap-1.5">
           <SearchBoxSection />
         </div>
       </div>
--- poddley_client/components/NavTitle/NavTitle.vue
+++ poddley_client/components/NavTitle/NavTitle.vue
@@ -1,5 +1,5 @@
 <template>
   <a href="/" class="tw-flex tw-justify-start tw-text-gray-700" title="homeLink">
-    <IconsLogoIcon />
+    <IconsLogoIcon class = "tw-scale-90 md:tw-scale-100  tw-mt-1" />
   </a>
 </template>
