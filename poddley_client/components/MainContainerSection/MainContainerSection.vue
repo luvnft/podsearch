a61fc7a2afa369e3b5d58da6f5f1f6873930dc93
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-03 23:54:58 +0200

    styling fix

--- poddley_client/components/MainContainerSection/MainContainerSection.vue
+++ poddley_client/components/MainContainerSection/MainContainerSection.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="tw-bg-slate-50 tw-min-h-screen tw-pr-1 tw-pl-1 tw-pb-1 tw-pt-7 md:tw-p-7">
+  <main class="tw-bg-slate-50 tw-min-h-screen tw-pr-1 tw-pl-1 tw-pb-1 tw-pt-7 md:tw-p-7">
     <div class="container tw-max-w-screen-md">
       <slot />
     </div>
-  </div>
+  </main>
 </template>
