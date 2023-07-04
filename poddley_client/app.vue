3d9e08bc2f191fa3a2ac184f92d2b7d57d3ccc36
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-05 00:38:47 +0200

    ok

--- poddley_client/app.vue
+++ poddley_client/app.vue
@@ -1,6 +1,14 @@
 <template>
-  <NuxtLayout>
-    <NuxtLoadingIndicator />
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="wrapper">
+    <NuxtLayout>
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
+
+<style scoped>
+.wrapper{
+  zoom: 1.2;
+}
+</style>
