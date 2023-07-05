be64194411502e7af822395665da165de92dde3c
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-05 21:13:40 +0200

    ok

--- poddley_client/components/NavBarSection/NavBarSection.vue
+++ poddley_client/components/NavBarSection/NavBarSection.vue
@@ -2,7 +2,7 @@
   <nav class="bg-light">
     <div class="container tw-max-w-screen-md tw-px-3">
       <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-1 tw-pb-5 tw-pt-3">
-        <NavTitle class = "tw-scale-90" />
+        <NavTitle class = "tw-scale-[0.8]" />
         <SearchBoxSection />
       </div>
     </div>
