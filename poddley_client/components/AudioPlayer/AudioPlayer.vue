72267a475756e2bfe7b7cb7cf7d3ff46addb8285
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-07 01:54:14 +0200

    ok

--- poddley_client/components/AudioPlayer/AudioPlayer.vue
+++ poddley_client/components/AudioPlayer/AudioPlayer.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="row tw-flex tw-h-12 tw-w-full tw-flex-row tw-rounded-lg tw-bg-white tw-text-sm tw-shadow-sm tw-shadow-gray-400 tw-px-0">
+  <div class="row tw-flex tw-h-12 tw-w-full tw-flex-row tw-rounded-lg tw-bg-white tw-px-0 tw-text-sm tw-shadow-sm tw-shadow-gray-400">
+    <audio ref="audioRef" :src="props.audioLink" @timeupdate="" @loadeddata="" />
     <div class="col-2 tw-flex tw-h-full tw-items-center tw-justify-center tw-pr-0">
-      <button class="tw-flex tw-h-10 tw-w-10 tw-items-center tw-justify-center tw-rounded-full tw-fill-gray-700 tw-transition-all tw-duration-300 hover:tw-bg-gray-200">
-        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
-          <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
-        </svg>
+      <button class="tw-flex tw-h-10 tw-w-10 tw-items-center tw-justify-center tw-rounded-full tw-fill-gray-700 tw-transition-all tw-duration-300 hover:tw-bg-gray-200" @click="togglePlay">
+        <div v-if="!audioIsPlaying">
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+            <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
+          </svg>
+        </div>
+        <div v-if="audioIsPlaying">
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
+            <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path
+              d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"
+            />
+          </svg>
+        </div>
       </button>
     </div>
     <div class="col-3 tw-flex tw-h-full tw-items-center tw-justify-start tw-pl-0 tw-pr-0">
       <span>13:21/17:22</span>
     </div>
     <div class="col-3 tw-flex tw-h-full tw-items-center tw-justify-start tw-px-0">
-  <input type="range" min="0" max="100" value="0" class="tw-w-full middle-line darkgrey-dot" id="audioRange" />
-</div>
+      <input type="range" min="0" max="100" value="0" class="middle-line darkgrey-dot tw-w-full" id="audioRange" />
+    </div>
     <div class="col-2 tw-flex tw-h-full tw-items-center tw-justify-center">play</div>
     <div class="col-2 tw-flex tw-h-full tw-items-center tw-justify-center">play</div>
   </div>
@@ -24,6 +35,22 @@ const props = defineProps<{
   startTime: number;
   episodeTitle: string;
 }>();
+
+const audioRef: Ref<HTMLAudioElement | null> = ref(null);
+const audioIsPlaying: Ref<boolean> = ref(false);
+
+const togglePlay = () => {
+  if (audioRef.value) {
+    const audioElement = audioRef.value;
+    audioIsPlaying.value = !audioIsPlaying.value;
+
+    if (audioElement.paused || audioElement.ended) {
+      audioElement.play();
+    } else {
+      audioElement.pause();
+    }
+  }
+};
 </script>
 
 <style scoped>
