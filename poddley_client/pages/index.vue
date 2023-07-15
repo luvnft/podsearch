1021f23e8de7f919203edc0426fdaff402fe0467
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-15 23:32:54 +0200

    testing

--- poddley_client/pages/index.vue
+++ poddley_client/pages/index.vue
@@ -5,55 +5,31 @@
 <script lang="ts" setup>
 //Imports
 import { SearchResponse } from "~/types/SearchResponse";
-import axios from "axios";
+import TranscriptionService from "~/utils/services/TranscriptionsService";
 import { storeToRefs } from "pinia";
+import { debounce } from "../utils/tools/tools";
 import { useSearchStore } from "../store/searchStore";
 
 //Vars
 const searchStore = useSearchStore();
 const searchResults: Ref<SearchResponse> = ref({} as SearchResponse);
+const transcriptionService: TranscriptionService = new TranscriptionService();
 const { searchString } = storeToRefs(searchStore);
-
-//Creating Axios instance and Cancel Token
-const CancelToken = axios.CancelToken;
-let cancel;
 
 //Initialization function
 async function makeSearch(string: string) {
-  if (cancel !== undefined) {
-    cancel();
-  }
-
   console.log("Triggered");
   searchStore.setLoadingState(true);
-
-  try {
-    const response = await axios.get("/transcriptions/search/", {
-      baseURL: "https://api.poddley.com",
-      cancelToken: new CancelToken(function executor(c: any) {
-        cancel = c;
-      }),
-      params: {
-        searchString: string,
-      },
-    });
-
-    searchResults.value = response.data;
-    searchStore.setLoadingState(false);
-  } catch (error: any) {
-    if (axios.isCancel(error)) {
-      console.log("Request canceled", error.message);
-    } else {
-      // handle error
-    }
-  }
+  searchResults.value = await transcriptionService.search(string);
+  searchStore.setLoadingState(false);
+  console.log(searchResults.value);
 }
 
-const throttledSearch = _Throttle(makeSearch, 300); // throttling the search to once every 300ms
+const debouncedSearch = debounce(makeSearch, 50);
 
 //Running
-watch(searchString, throttledSearch);
+watch(searchString, debouncedSearch);
 
 //Initial calls
-makeSearch("The following is a conversation with");
+makeSearch("The following is a conversation with attia");
 </script>
