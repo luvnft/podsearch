6709f1eca37cbde30ffe401286851b2601c1f4f9
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-20 19:14:13 +0200

    KKKKKKKKKK

--- poddley_client/pages/index.vue
+++ poddley_client/pages/index.vue
@@ -20,7 +20,7 @@ const { searchQuery } = storeToRefs(searchStore);
 const transcriptionService: TranscriptionService = new TranscriptionService();
 const utils: Utils = useUtils();
 const initialSearchQuery: SearchQuery = {
-  searchString: "The following is a conversation with attia",
+  searchString: "hello",
 };
 let worker;
 
