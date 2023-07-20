cee7dac037625cee51bec72c884bbe6df8591cd3
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-20 18:40:38 +0200

    debugging

--- poddley_client/components/NavTitle/NavTitle.vue
+++ poddley_client/components/NavTitle/NavTitle.vue
@@ -3,7 +3,7 @@
     <div class="tw-flex tw-h-full tw-w-full tw-items-center tw-justify-center tw-overflow-hidden sm:tw-hidden">
       <NuxtLink
         v-if="!loading"
-        href="/"
+        to="/"
         class="tw-flex tw-h-full tw-w-full tw-items-center tw-justify-center tw-rounded-md tw-p-0 tw-text-gray-400 hover:tw-bg-gray-100 hover:tw-text-gray-500 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-inset focus:tw-ring-gray-500"
       >
         <IconsLogoIcon class="tw-scale-[0.7] tw-mt-1.5" />
