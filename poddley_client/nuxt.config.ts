e1051a55736eee1000f064d8c156dfcf7600e4e3
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-21 20:07:21 +0200

    ok

--- poddley_client/nuxt.config.ts
+++ poddley_client/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       gzip: true,
     },
     minify: true,
-    preset: "cloudflare-pages",
+    preset: "cloudflare-pages-static",
   },
   pages: true,
   postcss: {
