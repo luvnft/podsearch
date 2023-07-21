4e5af0f054908f5751ae6b54621d430fa753a529
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-21 02:29:01 +0200

    ok

--- poddley_client/store/searchStore.ts
+++ poddley_client/store/searchStore.ts
@@ -1,11 +1,18 @@
 import { defineStore } from "pinia";
 import { SearchQuery } from "../types/SearchQuery";
 import { SearchResponse } from "types/SearchResponse";
+import { RouteLocationNormalized } from ".nuxt/vue-router";
+import { Utils } from "composables/useUtils";
 
+const route: RouteLocationNormalized = useRoute();
+const utils: Utils = useUtils();
 export const useSearchStore = defineStore("searchStore", () => {
   //Searchvariable
+  const routeBasedQuery = utils.decodeQuery(route.query?.searchQuery);
+
+  const query: SearchQuery = routeBasedQuery ? routeBasedQuery : searchQuery.value;
   const searchQuery: Ref<SearchQuery> = ref({
-    searchString: "hello there",
+    searchString: routeBasedQuery,
   });
   console.log("From pinia: ", searchQuery.value);
   const loading: Ref<boolean> = ref(false);
