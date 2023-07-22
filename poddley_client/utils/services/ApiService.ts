6394b611215a279021a65a7dc4300fa14b58d1e8
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-22 18:39:50 +0200

    Ok

--- poddley_client/utils/services/ApiService.ts
+++ poddley_client/utils/services/ApiService.ts
@@ -2,7 +2,7 @@ export default class ApiService {
   protected BASE_URL: string;
 
   protected constructor() {
-    this.BASE_URL = globalThis.__env__.LUKA || "";
+    this.BASE_URL = globalThis.NUXT_API_BASE_URL || "";
   }
 
   protected getBaseUrl() {
