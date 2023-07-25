b528474cb034b3badba792327cbaed6d5392e6f2
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-25 20:54:03 +0000

    backend modifications and refactoring

--- poddley_api/types/SearchQuery.ts
+++ poddley_api/types/SearchQuery.ts
@@ -2,4 +2,10 @@ export interface SearchQuery {
   searchString: string | undefined;
   sort?: string[];
   filter?: string;
+  limit?: number;
+  facets?: string[];
+  attributesToRetrieve?: string[];
+  showMatchesPosition?: boolean;
+  hitsPerPage?: number;
+  page?: number;
 }
