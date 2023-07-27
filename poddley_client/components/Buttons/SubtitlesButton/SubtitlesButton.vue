95fe37a87fc072e2f57b2b04310b392ce602ca57
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-07-27 18:48:00 +0200

    Added subtitles toggle-button, added copyLinkToSegment button, fixed frontend issues with audio-subtitle sync and made ui improvements

---
+++ poddley_client/components/Buttons/SubtitlesButton/SubtitlesButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    :class="`items-center font-medium } tw-group tw-flex tw-h-6 tw-h-8 tw-w-6 tw-w-8 tw-items-center tw-justify-center tw-rounded-md
+    tw-border tw-border-gray-300 tw-bg-gray-100 tw-fill-gray-400 tw-p-0 tw-text-gray-400 tw-no-underline tw-shadow tw-ring-gray-400 hover:tw-bg-gray-100 active:tw-shadow-sm`"
+  >
+    <svg-icon name="subtitlesOn" class="tw-h-4 tw-text-gray-400" v-show="props.activated" />
+    <svg-icon name="subtitlesOff" class="tw-h-4 tw-text-gray-400" v-show="!props.activated" />
+  </button>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  activated: boolean;
+}>();
+</script>
