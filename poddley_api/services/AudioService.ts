3cfab7c9e2eda5fefa411573641d64dc60f067a3
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-01 12:45:57 +0000

    Added backend stuff needed to send audio file, transcribe it and then get a text string in return mate

--- poddley_api/services/AudioService.ts
+++ poddley_api/services/AudioService.ts
@@ -1,59 +1,41 @@
-import { exec } from "child_process";
+import ffmpeg from "fluent-ffmpeg";
 import fs from "fs";
 import util from "util";
+import { WhisperCppTranscriptionType, WhisperCppTranscription } from "../types/WhisperCppTranscriptionType";
 
 class AudioService {
   public async processAudioFile(file: Express.Multer.File): Promise<string> {
-    const execPromisified = util.promisify(exec);
     const unlinkPromisified = util.promisify(fs.unlink);
 
     // Define the name and path of the output file
     const outputFilePath = `${file.path}.wav`;
 
-    // Command to convert the input file to 16bit .wav format
-    const convertCommand = `ffmpeg -i ${file.path} -ar 16000 -ac 1 -c:a pcm_s16le ${outputFilePath}`;
-
     try {
       // Convert the file
-      const { stdout: convertStdout, stderr: convertStderr } = await execPromisified(convertCommand);
-
-      if (convertStderr) {
-        console.error(`Error converting file: ${convertStderr}`);
-        throw new Error("Error converting file");
-      }
-
-      console.log(`Output of file conversion: ${convertStdout}`);
+      await new Promise((resolve, reject) => {
+        ffmpeg(file.path).outputOptions("-ar", "16000", "-ac", "1", "-c:a", "pcm_s16le").save(outputFilePath).on("end", resolve).on("error", reject);
+      });
 
       // Run the main command on the converted file
+      const execPromisified = util.promisify(require("child_process").exec);
       const mainCommand = `../whisper.cpp/main -m ../whisper.cpp/models/ggml-base.en.bin ${outputFilePath} -oj`;
       const { stdout: mainStdout, stderr: mainStderr } = await execPromisified(mainCommand);
 
-      if (mainStderr) {
-        console.error(`Error: ${mainStderr}`);
-        throw new Error("Error processing file");
+      // Check if file was saved as that is a sign of it being successful using outputFilePath
+      let whisperCppTranscriptionType: WhisperCppTranscriptionType;
+      if (fs.existsSync(outputFilePath)) {
+        //Then parse the object like this
+        const jsonData = fs.readFileSync(outputFilePath + ".json", 'utf-8');
+        whisperCppTranscriptionType = JSON.parse(jsonData);
+      } else {
+        throw new Error("Error: Output file does not exist");
       }
-
-      console.log(`Output: ${mainStdout}`);
-
-      // Parse the output as JSON.
-      let outputJson;
-      try {
-        outputJson = JSON.parse(mainStdout);
-      } catch (error) {
-        console.error("Error parsing JSON output:", error); 
-        throw new Error("Error parsing JSON output");
-      }
-
-      console.log("Parsed JSON:", outputJson);
 
       // Delete the original and converted files
-      await Promise.all([
-        unlinkPromisified(file.path),
-        unlinkPromisified(outputFilePath)
-      ]);
+      await Promise.all([unlinkPromisified(file.path), unlinkPromisified(outputFilePath)]);
 
-      return "File processed and deleted successfully";
-
+      const stringToReturn: string[] = whisperCppTranscriptionType.transcription.map((transcription: WhisperCppTranscription) => transcription.text);
+      return stringToReturn.join(" ").trim() || "";
     } catch (error: any) {
       console.error(`Error: ${error}`);
       throw new Error(error.message);
