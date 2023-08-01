1c82660b6c912b8aa42cc41496c3508ec902f18c
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-01 16:13:37 +0000

    performance improvements

--- poddley_api/services/AudioService.ts
+++ poddley_api/services/AudioService.ts
@@ -1,46 +1,46 @@
 import ffmpeg from "fluent-ffmpeg";
 import fs from "fs";
 import util from "util";
+import { exec } from "child_process";
 import { WhisperCppTranscriptionType, WhisperCppTranscription } from "../types/WhisperCppTranscriptionType";
+
+const unlinkPromisified = util.promisify(fs.unlink);
+const execPromisified = util.promisify(exec);
 
 class AudioService {
   public async processAudioFile(file: Express.Multer.File): Promise<string> {
-    const unlinkPromisified = util.promisify(fs.unlink);
-
     // Define the name and path of the output file
     const outputFilePath = `${file.path}.wav`;
 
     try {
       // Convert the file
-      await new Promise((resolve, reject) => {
-        ffmpeg(file.path).outputOptions("-ar", "16000", "-ac", "1", "-c:a", "pcm_s16le").save(outputFilePath).on("end", resolve).on("error", reject);
-      });
+      await this.convertAudio(file.path, outputFilePath);
 
       // Run the main command on the converted file
-      const execPromisified = util.promisify(require("child_process").exec);
       const mainCommand = `../whisper.cpp/main -m ../whisper.cpp/models/ggml-base.en.bin ${outputFilePath} -oj`;
-      const { stdout: mainStdout, stderr: mainStderr } = await execPromisified(mainCommand);
+      await execPromisified(mainCommand);
 
-      // Check if file was saved as that is a sign of it being successful using outputFilePath
-      let whisperCppTranscriptionType: WhisperCppTranscriptionType;
-      if (fs.existsSync(outputFilePath)) {
-        //Then parse the object like this
-        const jsonData = fs.readFileSync(outputFilePath + ".json", 'utf-8');
-        whisperCppTranscriptionType = JSON.parse(jsonData);
-      } else {
-        throw new Error("Error: Output file does not exist");
-      }
+      const jsonData = fs.readFileSync(outputFilePath + ".json", "utf-8");
+      const whisperCppTranscriptionType: WhisperCppTranscriptionType = JSON.parse(jsonData);
 
       // Delete the original and converted files
       await Promise.all([unlinkPromisified(file.path), unlinkPromisified(outputFilePath)]);
 
-      const stringToReturn: string[] = whisperCppTranscriptionType.transcription.map((transcription: WhisperCppTranscription) => transcription.text);
-      return stringToReturn.join(" ").trim() || "";
+      return this.extractTranscriptionText(whisperCppTranscriptionType);
     } catch (error: any) {
-      console.error(`Error: ${error}`);
       throw new Error(error.message);
     }
   }
-}
+
+  private async convertAudio(inputPath: string, outputPath: string): Promise<void> {
+    await new Promise((resolve, reject) => {
+      ffmpeg(inputPath).outputOptions("-ar", "16000", "-ac", "1", "-c:a", "pcm_s16le").save(outputPath).on("end", resolve).on("error", reject);
+    });
+  } 
+
+  private extractTranscriptionText(transcriptionType: WhisperCppTranscriptionType): string {
+    return transcriptionType.transcription.map((transcription: WhisperCppTranscription) => transcription.text).join(" ").trim() || "";
+  }
+}  
 
 export default AudioService;
