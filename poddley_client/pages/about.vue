27f8996c23987f2eaaeb138f32211ae7a50d1898
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-06 20:26:55 +0200

    testing

--- poddley_client/pages/about.vue
+++ poddley_client/pages/about.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="mt-3 flex w-full flex-col items-center justify-center text-gray-900">
     <h1>About</h1>
-    {{ 
-      $colorMode.value
-    }}
     <p>We like podcasts</p>
   </div>
 </template>
