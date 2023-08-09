8b93f7f3e05d853e8828670e8ec150b651d5c088
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-09 23:40:07 +0200

    DarkMode done

--- poddley_client/components/DarkMode/DarkMode.vue
+++ poddley_client/components/DarkMode/DarkMode.vue
@@ -2,19 +2,22 @@
   <div>
     <div class="flex h-full w-full items-center justify-center overflow-hidden sm:hidden">
       <button
-        class="text-gray-400 flex h-full w-full items-center justify-center rounded-md p-2 hover:text-gray-500 hover:bg-gray-100 focus:ring-gray-500 hover:border-none focus:outline-none focus:ring-2 focus:ring-inset"
+        class="group text-gray-400 flex h-full w-full items-center justify-center rounded-md p-2 hover:text-gray-500 hover:bg-gray-100 focus:ring-gray-500 hover:border-none focus:outline-none focus:ring-2 focus:ring-inset"
         @click="toggleColorMode"
         aria-hidden="true"
       >
-        <svg-icon v-show="isDark" name="sun" class="min-h-7 mt-0 block h-5 max-h-7 w-4/6 scale-[0.85] fill-gray-400 group-hover:fill-gray-500" aria-hidden="true" />
-        <svg-icon v-show="!isDark" name="moon" class="min-h-7 mt-0 block h-5 max-h-7 w-4/6 scale-[0.85] fill-gray-400 group-hover:fill-gray-500" aria-hidden="true" />
+        <div v-show="isDark">
+          <svg-icon name="sun" class="block h-full w-full scale-[0.70] fill-gray-500 group-hover:fill-gray-600 dark:group-hover:fill-gray-400" aria-hidden="true" />
+        </div>
+        <div v-show="!isDark">
+          <svg-icon name="moon" class="block h-full w-full scale-[0.70] fill-gray-500 group-hover:fill-gray-600" aria-hidden="true" />
+        </div>
       </button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useSearchStore } from "../../store/searchStore";
 const colorMode = useColorMode();
 const isDark = computed(() => colorMode.value === "dark");
 const toggleColorMode = () => {
