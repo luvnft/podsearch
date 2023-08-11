8db52367750d6ab5d8da511f1cf622dfae039861
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-11 08:31:14 +0200

    top margin no results text

--- poddley_client/components/SearchResults/SearchResults.vue
+++ poddley_client/components/SearchResults/SearchResults.vue
@@ -1,9 +1,10 @@
 <template>
-  <ul class="row m-0 flex justify-center gap-y-5 p-0">
+  <ul class="row m-0 flex justify-center gap-y-5 p-0" v-if="searchEntries.length > 0">
     <li class="col-12 max-w-md p-0 md:p-2" v-for="(searchEntry, index) in props.searchEntries">
       <SearchEntry :searchEntry="searchEntry" :key="searchEntry.id" />
     </li>
   </ul>
+  <p class="text-gray-800 text-center mt-4">No results for the given search query.</p>
 </template>
 
 <script lang="ts" setup>
