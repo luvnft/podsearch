2d1007eebe599f1accfe92d54b0f73fc8a4accda
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-13 02:04:26 +0200

    Added poddley-email forwarding to luka_momcilovic@hotmail.com

--- poddley_client/pages/about.vue
+++ poddley_client/pages/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-3 flex w-full flex-col items-center justify-center text-gray-900">
     <div class="max-w-3xl mx-auto p-6">
-      <h1 class="text-3xl font-bold mb-4">About Us - Poddley Podcast App</h1>
+      <h1 class="text-3xl font-bold mb-4">About Poddley</h1>
       <p class="mb-6">Welcome to Poddley, your ultimate destination for exploring the spoken world of podcasts like never before. Founded by the dynamic duo of Igor Momcilovic and Luka Momcilovic, Poddley is the culmination of our passion for technology, curiosity, and the power of human expression.</p>
 
       <div class="mb-6">
