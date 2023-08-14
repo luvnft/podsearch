28bb764f705c552a10d836a9d46ea0028f8af792
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-15 01:12:31 +0200

    design stuff

--- poddley_client/components/MainContainerSection/MainContainerSection.vue
+++ poddley_client/components/MainContainerSection/MainContainerSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="min-h-screen md:bg-slate-50 bg-white">
+  <main class="min-h-screen bg-white">
     <div class="container max-w-screen-md p-0 md:px-2">
       <slot />
     </div>
--- poddley_client/components/NavBarSection/NavBarSection.vue
+++ poddley_client/components/NavBarSection/NavBarSection.vue
@@ -1,19 +1,19 @@
 <template>
-  <Disclosure as="nav" class="bg-white border-gray-200 z-40 border-b pb-0 pt-0">
+  <Disclosure as="nav" class="bg-white z-40 pb-0 py-2">
     <div class="mx-auto h-full w-full px-0 sm:px-6 lg:px-8">
       <div class="relative flex h-full w-full justify-between">
         <div class="my-0 flex h-full w-full flex-1 items-center justify-between sm:items-stretch sm:justify-start">
-          <div class="container h-full w-full flex-shrink-0 items-center justify-between p-0">
-            <div class="row flex h-full w-full flex-nowrap items-center justify-between">
-              <div class="flex h-12 w-full sm:hidden">
+          <div class="h-full w-full flex-shrink-0 items-center justify-between p-0 m-0 ">
+            <div class="row flex h-full w-full flex-nowrap items-center justify-between m-0">
+              <div class="flex h-12 w-full sm:hidden justify-center">
                 <BurgerMenu class="col-2 flex h-full items-center justify-center" />
                 <DarkMode class="col-2 flex h-full items-center justify-center" />
                 <NavTitle class="col-4 flex h-full items-center justify-center" />
                 <SearchBox class="col-2 flex h-full items-center justify-center p-0" @click="toggleSearchSection" :openSearchSection="openSearchSection" />
                 <ButtonsMicrophoneButton class="col-2 flex h-full items-center justify-center" />
               </div>
-              <div class="hidden h-12 w-full sm:flex">
-                <DesktopNavBar class="h-full w-full" />
+              <div class="hidden h-12 w-full sm:flex justify-evenly ">
+                <DesktopNavBar class="h-full w-full m-0 justify-evenly" />
               </div>
             </div>
           </div>
@@ -30,15 +30,6 @@
 import { onKeyUp } from "@vueuse/core";
 import { vOnClickOutside } from "@vueuse/components";
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
-
-const navigation = [
-  { name: "Home", href: "/", current: true, external: false },
-  { name: "About", href: "about", current: false, external: false },
-  { name: "Contact", href: "/contact", current: false, external: false },
-  { name: "Donate", href: "https://www.buymeacoffee.com/poddley", current: false, external: false },
-];
-
-const navigateToExternal = (link: string) => (window.location.href = link);
 
 const openSearchSection: Ref<boolean> = ref(false);
 const openBurgerMenu: Ref<boolean> = ref(false);
