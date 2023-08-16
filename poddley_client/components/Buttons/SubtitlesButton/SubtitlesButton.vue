01e9f018d09ccdf261788b36dcff8b067ad7b40a
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-16 23:59:35 +0200

    ok

--- poddley_client/components/Buttons/SubtitlesButton/SubtitlesButton.vue
+++ poddley_client/components/Buttons/SubtitlesButton/SubtitlesButton.vue
@@ -1,10 +1,10 @@
 <template>
   <button
-    :class="`} group text-neutral-400 bg-neutral-100 border-neutral-300 ring-neutral-400 flex h-6 h-8 w-6 w-8 items-center
+    :class="`} group text-neutral-400 bg-neutral-100 border-neutral-300 ring-neutral-400 flex h-10 w-10 items-center
     items-center justify-center rounded-md border fill-neutral-400 p-0 font-medium no-underline shadow-sm hover:bg-neutral-100 active:shadow-sm`"
   >
-    <svg-icon name="subtitlesOn" class="text-gray-400 h-4" v-show="props.activated" />
-    <svg-icon name="subtitlesOff" class="text-gray-400 h-4" v-show="!props.activated" />
+    <svg-icon name="subtitlesOn" class="text-gray-400 h-5" v-show="props.activated" />
+    <svg-icon name="subtitlesOff" class="text-gray-400 h-5" v-show="!props.activated" />
   </button>
 </template>
 
