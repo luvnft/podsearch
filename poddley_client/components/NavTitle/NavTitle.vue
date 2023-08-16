7ff01805ac45a8fec30aeeb67561fa23e3303a00
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-16 20:51:59 +0200

    design fixes

--- poddley_client/components/NavTitle/NavTitle.vue
+++ poddley_client/components/NavTitle/NavTitle.vue
@@ -1,9 +1,10 @@
 <template>
   <GenericButton
-    class="text-gray-400 flex h-12 w-28 sm:w-24 items-center justify-center rounded-md fill-gray-600 p-0 hover:text-gray-500 focus:ring-gray-500 hover:fill-gray-700 focus:outline-none focus:ring-2 focus:ring-inset dark:fill-neutral-400 dark:hover:fill-gray-300"
+    class="text-gray-400 flex h-12 w-28 items-center justify-center rounded-md fill-gray-600 p-0 hover:text-gray-500 focus:ring-gray-500 hover:fill-gray-700 focus:outline-none focus:ring-2 focus:ring-inset dark:fill-neutral-400 dark:hover:fill-gray-300 sm:w-24"
+    @click="toHomeClick"
   >
     <div :class="`flex items-center justify-center ${!loading || 'hidden'}`">
-      <svg-icon name="logo" class="min-h-6 mt-1 block max-h-7 w-4/6" aria-hidden="true" />
+      <svg-icon name="logo" class="min-h-6 mt-1 block max-h-7 w-4/6 fill-gray-500 brightness-90 dark:fill-neutral-300" aria-hidden="true" />
     </div>
     <IconsSpinnerIcon v-show="loading === true" />
   </GenericButton>
