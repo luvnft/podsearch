18d2dd29c2032d5718f4516ef0d57ff75c4f8b67
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-16 23:52:16 +0200

    ok

--- poddley_client/components/MoreTextCard/MoreTextCard.vue
+++ poddley_client/components/MoreTextCard/MoreTextCard.vue
@@ -1,57 +1,42 @@
 <template>
-  <TransitionRoot as="template" :show="moreTextModalOpen">
-    <Dialog as="div" class="fixed z-10 h-full w-full " @close="toggleMoreTextModal">
-      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
-        <div class="bg-gray-800 dark:bg-gray-100 fixed inset-0 bg-opacity-90 transition-opacity" />
-      </TransitionChild>
-
-      <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
-          <TransitionChild
-            as="template"
-            enter="ease-out duration-300"
-            enter-from="opacity-0 translate-y-4 sm:translate-y-0"
-            enter-to="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leave-from="opacity-100 translate-y-0 sm:scale-100"
-            leave-to="opacity-0 translate-y-4 sm:translate-y-0"
-          >
-            <DialogPanel class="bg-white relative transform overflow-hidden rounded-lg px-4 pb-4 pt-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
-              <div>
-                <div class="bg-blue-100 mx-auto flex h-12 w-12 items-center justify-center rounded-full">
-                  <ChatBubbleBottomCenterTextIcon class="text-neutral-900 h-6 w-6" aria-hidden="true" />
-                </div>
-                <div class="mt-3 text-center sm:mt-5">
-                  <DialogTitle as="h3" class="text-gray-900 text-lg font-medium leading-6">{{ props.podcastName }}</DialogTitle>
-                  <div class="mt-2">
-                    <p class="text-gray-500 text-sm">{{ props.podcastTest }}</p>
-                  </div>
-                </div>
-              </div>
-              <div class="mt-5 sm:mt-6">
-                <button
-                  type="button"
-                  class="text-white bg-indigo-600 inline-flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-base font-medium shadow-sm hover:bg-indigo-700 focus:ring-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-sm"
-                  @click="toggleMoreTextModal"
-                >
-                  Go back
-                </button>
-              </div>
-            </DialogPanel>
-          </TransitionChild>
+  <div id="defaultModalWrapper" class="fixed inset-0 flex items-center justify-center z-50 m-3">
+    <div id="defaultModal" tabindex="-1" aria-hidden="true" class="relative w-full max-w-2xl max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
+      <!-- Modal content -->
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- Modal header -->
+        <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+          <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+              {{podcastName}}
+          </h3>
+          <button @click="closeDialog()" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
+        <!-- Modal body -->
+        <div class="p-6 space-y-6 max-h-[calc(100vh-12rem)] overflow-y-auto">
+          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              <p v-html="podcastTest.trim()" class = "ml-0 my-0 mr-0" />
+          </p>
+        </div>
+        <!-- Modal footer -->
+        <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <button data-modal-hide="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">IDK</button>
+          <button @click="closeDialog()" data-modal-hide="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Go back</button>
         </div>
       </div>
-    </Dialog>
-  </TransitionRoot>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from "@headlessui/vue";
-import { ChatBubbleBottomCenterTextIcon } from "@heroicons/vue/24/outline";
+
 const props = defineProps<{
-  toggleMoreTextModal: () => void;
-  moreTextModalOpen: boolean;
+  closeDialog: () => void;
   podcastTest: any;
   podcastName: string;
 }>();
+
 </script>
