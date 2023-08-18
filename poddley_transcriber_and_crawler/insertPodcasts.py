38bd07d338517ffa9216aec5cb21a45aad8151c7
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-17 23:17:02 -0700

    some good old refactoring...

--- poddley_transcriber_and_crawler/insertPodcasts.py
+++ poddley_transcriber_and_crawler/insertPodcasts.py
@@ -10,12 +10,7 @@ import time
 async def insertPodcasts(objectsToInsert):
     prisma = Prisma()
     await prisma.connect()
-    imagesPath = "https://images.poddley.com/"
     
-    # print("Inserting podcasts and stuff, deleting already existent onces")
-    # await prisma.podcast.delete_many()
-    # await prisma.episode.delete_many()
-    # await prisma.transcription.delete_many()
     print("HERE=======================>")
     
     for object in objectsToInsert:
@@ -27,17 +22,9 @@ async def insertPodcasts(objectsToInsert):
     for object in objectsToInsert:
         try:
             print("Inserting podcast: ", object["title"])
-            titleTrimmed = object["title"]
-            titleLowerCased = titleTrimmed.lower()
-            titleSplitted = titleLowerCased.split(" ")
-            titleSplittedReplaced = [re.sub(r'[^a-z]', '', e) for e in titleSplitted]
-            titleSplittedJoined = "-".join(titleSplittedReplaced)
-            newImageName = titleSplittedJoined + "-podcastImage"
-            object["imageUrl"] = imagesPath + newImageName + ".webp"
             await prisma.podcast.create(data=object)
 
         except Exception as e:
             print("ERROR: ", e)
-    subprocess.run(["node", "downscalePodcastImages.js"], check=True)
 
     print("Done")
--- poddley_transcriber_and_crawler/transcribeAndDump.py
+++ poddley_transcriber_and_crawler/transcribeAndDump.py
@@ -1,78 +1,69 @@
 from prisma import Prisma
-import whisper
+import whisperx
 import asyncio
 import transcribeAndDumpIt
-from faster_whisper import WhisperModel
 
 model_size = "tiny.en"
+device = "cuda"
+compute_type = "float16"  # change to "int8" if low on GPU mem (may reduce accuracy)
+
+async def findOneEpisode(prismaInstance):
+    episode = await prismaInstance.episode.find_first(
+        where={"isTranscribed": False, "beingTranscribed": False, "causedError": False}
+    )
+    return episode
+
+
 async def main():
     prisma = Prisma()
     await prisma.connect()
-    
-    # model = whisper.load_model("tiny.en")
-    model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+    # 1. Transcribe with original whisper (batched)
+    model = whisperx.load_model(model_size, device, compute_type=compute_type)
     print("Using the model:", model_size)
 
     # Looping over all the episodes in the database
     while True:
-        # Get the rows of episodes for that iteration skip value and take value
-        episode = None
         try:
-            episode = await prisma.episode.find_first(
-                where = {
-                    "isTranscribed": False,
-                    "beingTranscribed": False,
-                    "causedError": False
-                }
-            )
-            
-            print("Found this one: ", episode.id)
+            # Get the rows of episodes for that iteration skip value and take value
+            episode = await findOneEpisode(prisma)
+            episode = dict(episode)
             
             if episode == None:
                 # No more episodes to transcribe, so finishing.
                 break
-            
-            # Updating the episode to beingTranscribed to avoid collisions.
-            await prisma.episode.update(
-                where = {
-                    "id": episode.id
-                },
-                data = {
-                    "beingTranscribed": True
-                }
-            )
+            else:
+                # Updating the episode to beingTranscribed to avoid collisions.
+                print("Found this one: ", episode.id)
+                await prisma.episode.update(
+                    where={"id": episode.id}, data={"beingTranscribed": True}
+                )
 
-            # Transcribe and Insert
-            print("Transcribing it from dumpIT", episode.episodeTitle)
-            await transcribeAndDumpIt.transcribeAndDumpIt(episode, model)
-            print("Done with episode and updating", episode.id)
-            print("Done with one, next!")
-            
+                # Transcribe and Insert
+                print("Transcribing:", episode.episodeTitle)
+                await transcribeAndDumpIt.transcribeAndDumpIt(episode, model)
+                print("Done with episode: ", episode.id, "=> Next!")
         except Exception as e:
             print("SHIT SHIT SHIT, error occurred", str(e))
             # If the transcribing fails, then set the beingTranscribed column to false
             await prisma.episode.update(
-                where = {
-                    "id": episode.id
-                },
-                data = {
+                where={"id": episode.id},
+                data={
                     "beingTranscribed": False,
                     "isTranscribed": False,
-                    "causedError": True
-                }
-            )
-            await prisma.segment.delete_many(
-                where = {
-                    "belongsToEpisodeGuid": episode.episodeGuid
+                    "causedError": True,
                 },
             )
-            await prisma.transcription.delete(
-                where = {
-                    "belongsToEpisodeGuid": episode.episodeGuid
-                }
+            # Then delete any segments which might have been added that belong to that given episodeGuid
+            await prisma.segment.delete_many(
+                where={"belongsToEpisodeGuid": episode.episodeGuid},
             )
+            # As well with the transcriptions
+            await prisma.transcription.delete(
+                where={"belongsToEpisodeGuid": episode.episodeGuid}
+            )
+
 
 if __name__ == "__main__":
     # Run the main function
-    # For windows
     asyncio.run(main())
