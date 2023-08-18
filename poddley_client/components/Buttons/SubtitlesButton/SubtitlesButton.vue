9e2c30863cd8c74a236ac48d08859242d0eebfe2
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-08-18 12:18:52 +0200

    more fixes

--- poddley_client/components/Buttons/SubtitlesButton/SubtitlesButton.vue
+++ poddley_client/components/Buttons/SubtitlesButton/SubtitlesButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    :class="`group text-neutral-400 bg-neutral-100 border-neutral-300 ring-neutral-400 flex h-9 w-9 
+    :class="`group text-gray-400 bg-neutral-100 border-neutral-300 ring-neutral-400 flex h-9 w-9 
     items-center justify-center rounded-lg border p-0 font-medium no-underline shadow-sm hover:bg-neutral-100 active:shadow-sm`"
   >
     <svg-icon name="subtitlesOn" class="h-5 fill-gray-400" v-show="props.activated" />
