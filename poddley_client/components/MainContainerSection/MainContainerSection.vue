8e07e2eaf069d039de952822f6b2ca7970f73803
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-03 23:47:40 +0200

    design fixes

--- poddley_client/components/MainContainerSection/MainContainerSection.vue
+++ poddley_client/components/MainContainerSection/MainContainerSection.vue
@@ -1,7 +1,7 @@
 <template>
-    <main class="bg-white min-h-screen sm:px-3 pb-10">
-        <div class="container-fluid max-w-screen-xl px-0">
-            <slot />
-        </div>
-    </main>
+  <main class="bg-white min-h-screen px-3 pb-10">
+    <div class="container-fluid max-w-screen-xl px-0">
+      <slot />
+    </div>
+  </main>
 </template>
