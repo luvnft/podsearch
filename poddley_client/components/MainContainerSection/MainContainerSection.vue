a551679f74e587fe848bf51f2e4798b8854417e7
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-04 11:22:42 +0200

    fixes

--- poddley_client/components/MainContainerSection/MainContainerSection.vue
+++ poddley_client/components/MainContainerSection/MainContainerSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-white min-h-screen px-3 pb-10">
+  <main class="bg-white min-h-screen xs:px-0 sm:px-2 xs:py-0 sm:py-2">
     <div class="container-fluid max-w-screen-xl px-0">
       <slot />
     </div>
