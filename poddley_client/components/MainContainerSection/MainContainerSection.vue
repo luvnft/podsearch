a598725b10bfc3194e96e45d67f9b58c047a2950
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-05 19:45:53 +0200

    audio player done

--- poddley_client/components/MainContainerSection/MainContainerSection.vue
+++ poddley_client/components/MainContainerSection/MainContainerSection.vue
@@ -3,13 +3,5 @@
     <div class="container-fluid max-w-screen-xl px-0">
       <slot />
     </div>
-    <AudioPlayer v-if="playing" />
   </main>
 </template>
-
-<script setup lang="ts">
-import { usePlayerStore } from "../../store/playerStore";
-import { storeToRefs } from "pinia";
-const playerStore = usePlayerStore();
-const { playing } = storeToRefs(playerStore);
-</script>
