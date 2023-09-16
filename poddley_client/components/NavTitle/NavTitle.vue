955e468e721bf6bc97981ce1f7f4685971fa6c7c
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-17 01:42:39 +0200

    revert

--- poddley_client/components/NavTitle/NavTitle.vue
+++ poddley_client/components/NavTitle/NavTitle.vue
@@ -1,13 +1,13 @@
 <template>
-  <NuxtLink
+  <a
     class="flex items-center justify-center h-12 p-0 text-gray-400 rounded-md w-28 hover:text-gray-500 focus:ring-gray-500 hover:fill-gray-700 focus:outline-none focus:ring-2 focus:ring-inset sm:w-24"
-    to="/"
+    href="/"
   >
     <div :class="`flex items-center justify-center ${!loading || 'hidden'}`">
       <svg-icon name="logo" class="block w-4/6 mt-1 min-h-6 max-h-7 fill-gray-600 brightness-90 dark:fill-blue-100" aria-hidden="true" />
     </div>
     <IconsSpinnerIcon v-show="loading === true" />
-  </NuxtLink>
+  </a>
 </template>
 
 <script setup lang="ts">
