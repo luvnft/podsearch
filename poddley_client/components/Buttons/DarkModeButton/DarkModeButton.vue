3f6fa551f7d13e3c9c7c40d81081f63a4c3c2faa
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-24 18:10:03 +0200

    ok

--- poddley_client/components/Buttons/DarkModeButton/DarkModeButton.vue
+++ poddley_client/components/Buttons/DarkModeButton/DarkModeButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg-icon name="microphone" class="sun block h-full fill-gray-500 group-hover:fill-gray-500 dark:group-hover:fill-gray-400" aria-hidden="true" />
+    <svg-icon name="sun" class="sun block h-full fill-gray-500 group-hover:fill-gray-500 dark:group-hover:fill-gray-400" aria-hidden="true" />
 </template>
 <script setup lang="ts">
 const colorMode = useColorMode();
