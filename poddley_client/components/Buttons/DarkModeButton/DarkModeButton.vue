53b51eb253b963ec7c3f151808658ea737ee9bc9
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-24 17:51:30 +0200

    ooK

--- poddley_client/components/Buttons/DarkModeButton/DarkModeButton.vue
+++ poddley_client/components/Buttons/DarkModeButton/DarkModeButton.vue
@@ -1,13 +1,13 @@
 <template>
   <ButtonsGenericButton @click="toggleColorMode" class="aspect-square h-12 w-12">
-        <svg-icon name="sun" class="sun block h-full fill-gray-500 group-hover:fill-gray-500 dark:group-hover:fill-gray-400" aria-hidden="true" />
+    <svg-icon name="sun" class="sun block h-full fill-gray-500 group-hover:fill-gray-500 dark:group-hover:fill-gray-400" aria-hidden="true" />
   </ButtonsGenericButton>
 </template>
 <script setup lang="ts">
 const colorMode = useColorMode();
 
 const toggleColorMode = () => {
-console.log(colorMode);
+  console.log(colorMode);
   if (colorMode.unknown === false) {
     if (colorMode.preference === "system") {
       colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
