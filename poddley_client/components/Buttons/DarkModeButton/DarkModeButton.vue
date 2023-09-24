1ce67dc836c9824acc2093ef532b13a678750bbb
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-24 21:56:15 +0200

    final fix

--- poddley_client/components/Buttons/DarkModeButton/DarkModeButton.vue
+++ poddley_client/components/Buttons/DarkModeButton/DarkModeButton.vue
@@ -1,7 +1,7 @@
 <template>
   <ButtonsGenericButton @click="toggleColorMode" class="aspect-square h-12 w-12">
-    <div class="flex aspect-square h-12 w-12 items-center justify-center">
-        <svg-icon name="sun" class="sun block h-full scale-[0.55] fill-gray-500 group-hover:fill-gray-500 dark:group-hover:fill-gray-400" aria-hidden="true" />
+    <div class="h-full w-full">
+      <svg-icon name="sun" class="block h-full w-full scale-[0.6] fill-gray-400 group-hover:fill-gray-500" aria-hidden="true" />
     </div>
   </ButtonsGenericButton>
 </template>
