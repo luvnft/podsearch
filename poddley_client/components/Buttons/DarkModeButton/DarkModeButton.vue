5a4cba7f97d89d77f607fc882a21462c0f00d65c
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-24 22:04:43 +0200

    final tifx

--- poddley_client/components/Buttons/DarkModeButton/DarkModeButton.vue
+++ poddley_client/components/Buttons/DarkModeButton/DarkModeButton.vue
@@ -1,7 +1,37 @@
 <template>
   <ButtonsGenericButton @click="toggleColorMode" class="aspect-square h-12 w-12">
-    <div class="h-full w-full">
-      <svg-icon name="sun" class="block h-full w-full scale-[0.6] fill-gray-400 group-hover:fill-gray-500" aria-hidden="true" />
+    <div class="flex aspect-square h-12 w-12 items-center justify-center">
+      <div class="h-full w-full">
+        <svg-icon name="sun" class="sun block h-full w-full scale-[0.6] fill-gray-400 group-hover:fill-gray-500" aria-hidden="true" />
+      </div>
+      <div class="h-full w-full">
+        <svg-icon name="moon" class="moon block h-full w-full scale-[0.6] fill-gray-400 group-hover:fill-gray-500" aria-hidden="true" />
+      </div>
     </div>
   </ButtonsGenericButton>
 </template>
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const toggleColorMode = () => {
+  console.log(colorMode);
+  if (colorMode.unknown === false) {
+    if (colorMode.preference === "system") {
+      colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+    } else {
+      colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* This allows instant beforeRender iconSwitch 😎 */
+.light .sun {
+  display: none !important;
+}
+
+.dark .moon {
+  display: none !important;
+}
+</style>
