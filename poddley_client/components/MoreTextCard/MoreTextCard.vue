0219afba0b82fb812af5b95151e8a22fe1ea465d
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-26 18:57:06 +0200

    cleaning up, fixes etc

--- poddley_client/components/MoreTextCard/MoreTextCard.vue
+++ poddley_client/components/MoreTextCard/MoreTextCard.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="defaultModalWrapper" class="fixed inset-0 flex items-center justify-center z-50 m-3">
     <div id="defaultModal" tabindex="-1" aria-hidden="true" class="relative w-full max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
-      <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-        <!-- Modal header -->
         <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
               {{podcastName}}
@@ -15,13 +13,11 @@
             <span class="sr-only">Close modal</span>
           </button>
         </div>
-        <!-- Modal body -->
         <div class="p-6 space-y-6 max-h-[calc(100vh-10rem)] overflow-y-auto">
           <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
               <p v-html="podcastTest.trim()" class = "ml-0 my-0 mr-0" />
           </p>
         </div>
-        <!-- Modal footer -->
         <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
           <button data-modal-hide="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">IDK</button>
           <button @click="closeDialog()" data-modal-hide="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-base font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Go back</button>
