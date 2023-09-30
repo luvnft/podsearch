6ae6eb0d74cc156057e91100eccb4d0f516c3053
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-09-30 03:19:59 +0200

    a little buit more x-space

--- poddley_client/components/MainContainerSection/MainContainerSection.vue
+++ poddley_client/components/MainContainerSection/MainContainerSection.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="xs:px-0 xs:py-0 bg-white min-h-screen sm:py-1 px-0.5 lg:px-3.5">
-    <div class="container-fluid max-w-screen px-1 py-1">
+    <div class="container-fluid max-w-screen px-0.5 py-1">
       <slot />
     </div>
   </main>
