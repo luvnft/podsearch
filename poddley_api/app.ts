459a506d203d9318dfc5549dacd4b8be6d911303
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-01 00:58:07 +0000

    Some backend reverts and fixes

--- poddley_api/app.ts
+++ poddley_api/app.ts
@@ -1,45 +1,25 @@
 //Average stuff needed
 import express, { Express } from "express";
 import cors from "cors";
-import helmet from "helmet";
 import transcriptionsRouter from "./routes/transcriptions";
-import rateLimit from "express-rate-limit";
-import audioServiceRouter from "./routes/audio";
+import compression from "compression";
 
-//Limiter
-const apiCallLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: true, // Disable the `X-RateLimit-*` headers
-});
-
-// File upload limiter
-const fileUploadLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  max: 100, // Limit each IP to 100 file uploads per hour
-});
+process.env.NODE_ENV = 'production';
 
 //Setup
 const app: Express = express();
 const port: number = 3000;
 
-// Use helmet
-app.use(helmet()); 
-
-//Enable CORS for front-end use
+//Enable CORS for front-end use and compression
+app.use(compression());
 app.use(cors());
 
 //Enabling body
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-//Use the routes
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+ 
+//Use the routes 
 app.use("/transcriptions", transcriptionsRouter);
-app.use("/audio", fileUploadLimiter, audioServiceRouter);
-
-// Apply the rate limiting middleware to all requests
-// app.use(apiCallLimiter);
 
 //Listen on port
 app.listen(port, () => {
--- poddley_api/services/TranscriptionsService.ts
+++ poddley_api/services/TranscriptionsService.ts
@@ -4,7 +4,7 @@ import { SearchResponse, SearchResponseHit } from "../types/SearchResponse";
 import { SegmentResponse, SegmentHit } from "../types/SegmentResponse";
 import { PodcastResponse, PodcastHit } from "../types/PodcastResponse";
 import _ from "lodash";
-import { Index, MeiliSearch, MultiSearchParams, MultiSearchQuery, MultiSearchResponse, MultiSearchResult } from "meilisearch";
+import { Index, MeiliSearch, MultiSearchParams, MultiSearchResponse, MultiSearchResult } from "meilisearch";
 import { EpisodeHit, EpisodeResponse } from "../types/EpisodeResponse";
 import { PrismaClient } from "@prisma/client";
 import { SearchQuery } from "../types/SearchQuery";
@@ -51,6 +51,7 @@ class TranscriptionsService {
   //The search function (main one main use)
   public async search(searchQuery: SearchQuery): Promise<SearchResponse> {
     // MainQuery
+    console.log(searchQuery);
     let mainQuery: SearchParams = {
       attributesToHighlight: ["text"],
       highlightPreTag: '<span class="highlight">',
@@ -58,11 +59,13 @@ class TranscriptionsService {
       matchingStrategy: "last",
       q: searchQuery.searchString,
       filter: searchQuery.filter,
-      limit: 50,
+      limit: 12,
+      offset: searchQuery.offset || 0,
     };
 
     // Initial search
     let response: SearchResponse = await this.segmentSearch(mainQuery);
+    console.log(response);
 
     // Return response
     return response;
@@ -71,30 +74,41 @@ class TranscriptionsService {
   private async segmentSearch(searchParams: SearchParams): Promise<SearchResponse> {
     const startTime = new Date().getTime();
     // Search results => Perform it.
+    console.log("YES");
     let initialSearchResponse: SegmentResponse = await this.segmentsIndex.search(undefined, searchParams);
+    console.log("YES22");
+
     let searchResponse: SearchResponse = {
       query: searchParams.q || "",
       hits: [],
     };
 
+    console.log("NO");
     // Getting the podcast ids and episode ids to fetch them further as they are not part of the segmentObjects on the segmentsIndex
     const podcastIds: string[] = [...new Set(initialSearchResponse.hits.map((hit: SegmentHit) => hit.belongsToPodcastGuid))] as string[];
     const episodeIds: string[] = [...new Set(initialSearchResponse.hits.map((hit: SegmentHit) => hit.belongsToEpisodeGuid))] as string[];
 
+    console.log(podcastIds);
+    console.log(episodeIds);
     // Making a query for the podcast and episodes based on the ids connected to the segments
+    const [podcasts, episodes] = await Promise.all([this.searchPodcastsWithIds(podcastIds), this.searchEpisodesWithIds(episodeIds)]);
+    const podcastsMap: Map<string, PodcastHit> = new Map(podcasts.hits.map((podcast) => [podcast.podcastGuid, podcast]));
+    const episodesMap: Map<string, EpisodeHit> = new Map(episodes.hits.map((episode) => [episode.episodeGuid, episode]));
 
+    console.log("HUHUHUU");
     // Get surrounding hits or not depending on boolean flag
     let multiSearchParams: MultiSearchParams = {
-      queries: [this.getPodcastMultiQuery(podcastIds), this.getEpisodeMultiQuery(episodeIds)],
+      queries: [],
     };
     if (searchParams.getFullTranscript) {
     } else {
+      console.log("MAMMA MIA");
       initialSearchResponse.hits.map((segmentHit: SegmentHit) => {
         multiSearchParams.queries.push({
           indexUid: "segments", // Replace with the actual index name
           q: searchParams.q,
           filter: `start ${segmentHit.start} TO ${segmentHit.start + 300} AND belongsToEpisodeGuid = '${segmentHit.belongsToEpisodeGuid}'`,
-          limit: 25,
+          limit: 50,
           sort: ["start:asc"],
           attributesToHighlight: ["text"],
           highlightPreTag: '<span class="highlight">',
@@ -104,11 +118,7 @@ class TranscriptionsService {
       });
 
       let multiSearchResponse: MultiSearchResponse = await meilisearchConnection.multiSearch(multiSearchParams);
-      const podcastsMap: Map<string, PodcastHit> = new Map(multiSearchResponse.results[0].hits.map((podcastHit: any) => [podcastHit.podcastGuid, podcastHit]));
-      const episodesMap: Map<string, EpisodeHit> = new Map(multiSearchResponse.results[1].hits.map((episodeHit: any) => [episodeHit.episodeGuid, episodeHit]));
-      multiSearchResponse.results = multiSearchResponse.results.slice(2);
-      
-      console.log(podcastsMap);
+      console.log("OK");
       multiSearchResponse.results.forEach((result: MultiSearchResult<any>, index: number) => {
         let segmentHit: any = initialSearchResponse.hits[index];
         let postHits: SegmentHit[] = result.hits;
@@ -135,51 +145,48 @@ class TranscriptionsService {
           deviationTime: segmentHitEpisode.deviationTime || 0,
           subHits: postHits,
         };
+
+        if (searchResponseHitConstructed && searchResponseHitConstructed._formatted) {
+          let formattedData = searchResponseHitConstructed?.subHits?.map((item: SegmentHit) => {
+            let startTime = item.start;
+            let endTime = item.end;
+            let text = item._formatted.text;
+            let words = text.split(/ (?![^<]*>)/g);
+
+            // If bigger than 5 words, gotta make em smaller
+            if (words.length > 5) {
+              let segments = [];
+
+              // Calculate duration per word in the original segment
+              let durationPerWord = (endTime - startTime) / words.length;
+
+              let segmentStartTime = startTime; // Initialize segmentStartTime for the first segment
+
+              while (words.length) {
+                // Splice is cutting the words from 0 and removes 6 returning them, leaving words 6 words less
+                let segmentWords = words.splice(0, 6); // Adjust the '6' to disperse the words accordingly in the segments
+                let segmentEndTime = segmentStartTime + segmentWords.length * durationPerWord; // Getting segmentEndTime which will be the new segmentStartTime for the next segment based on the durationperWord
+
+                // Create and add the formatted segment to the segments array
+                segments.push(`<p><time>${convertSecondsToTime(segmentStartTime)}:</time> <i>${segmentWords.join(" ").trim()}</i></p>`);
+
+                // Update the segmentStartTime for the next segment
+                segmentStartTime = segmentEndTime;
+              }
+
+              return segments.join("");
+            }
+
+            // If not, let'se goooo
+            return `<p><time>${convertSecondsToTime(startTime)}:</time> <i>${text}</i></p>`;
+          });
+
+          searchResponseHitConstructed._formatted.text = formattedData?.join("") || "";
+          searchResponseHitConstructed.subHits = [];
+        }
         searchResponse.hits.push(searchResponseHitConstructed);
       });
     }
-
-    // Change the response to be correctly formatted
-    searchResponse.hits.forEach((responseHit: SearchResponseHit) => {
-      if (responseHit && responseHit._formatted) {
-        let formattedData = responseHit?.subHits?.map((item: SegmentHit) => {
-          let startTime = item.start;
-          let endTime = item.end;
-          let text = item._formatted.text;
-          let words = text.split(/ (?![^<]*>)/g);
-
-          // If bigger than 5 words, gotta make em smaller
-          if (words.length > 5) {
-            let segments = [];
-
-            // Calculate duration per word in the original segment
-            let durationPerWord = (endTime - startTime) / words.length;
-
-            let segmentStartTime = startTime; // Initialize segmentStartTime for the first segment
-
-            while (words.length) {
-              // Splice is cutting the words from 0 and removes 6 returning them, leaving words 6 words less
-              let segmentWords = words.splice(0, 6); // Adjust the '6' to disperse the words accordingly in the segments
-              let segmentEndTime = segmentStartTime + segmentWords.length * durationPerWord; // Getting segmentEndTime which will be the new segmentStartTime for the next segment based on the durationperWord
-
-              // Create and add the formatted segment to the segments array
-              segments.push(`<p><time>${convertSecondsToTime(segmentStartTime)}:</time> <i>${segmentWords.join(" ").trim()}</i></p>`);
-
-              // Update the segmentStartTime for the next segment
-              segmentStartTime = segmentEndTime;
-            }
-
-            return segments.join("");
-          }
-
-          // If not, let'se goooo
-          return `<p><time>${convertSecondsToTime(startTime)}:</time> <i>${text}</i></p>`;
-        });
-
-        responseHit._formatted.text = formattedData?.join("") || "";
-        responseHit.subHits = [];
-      }
-    });
 
     // Assign similarity score to all hits. If no searchString, nothing to calculate essentially
     if (searchParams.q) {
@@ -205,32 +212,6 @@ class TranscriptionsService {
     });
     // Return data
     return resData;
-  }
-
-  private getPodcastMultiQuery(podcastIds: string[]): MultiSearchQuery {
-    podcastIds = podcastIds.map((e) => `'${e}'`);
-    const filter: string = `podcastGuid=${podcastIds.join(" OR podcastGuid=")}`;
-    const podcastMultiSearchQuery: MultiSearchQuery = {
-      limit: podcastIds.length || 5,
-      filter: filter,
-      indexUid: "podcasts",
-      q: undefined,
-    };
-    // Return podcastMultiSearchQuery
-    return podcastMultiSearchQuery;
-  }
-
-  private getEpisodeMultiQuery(episodesIds: string[]): MultiSearchQuery {
-    episodesIds = episodesIds.map((e) => `'${e}'`);
-    const filter: string = `episodeGuid=${episodesIds.join(" OR episodeGuid=")}`;
-    const episodeMultiSearchQuery: MultiSearchQuery = {
-      limit: episodesIds.length || 5,
-      filter: filter,
-      indexUid: "episodes",
-      q: undefined,
-    };
-    // Return episodeMultiSearchQuery
-    return episodeMultiSearchQuery;
   }
 
   private async searchEpisodesWithIds(episodesIds: string[]): Promise<EpisodeResponse> {
