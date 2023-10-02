6dc7b8d22f99049697b473cc14b8c262e5631fbd
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-03 00:13:54 +0200

    ok

--- poddley_client/components/Buttons/SubtitlesButton/SubtitlesButton.vue
+++ poddley_client/components/Buttons/SubtitlesButton/SubtitlesButton.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="flex w-9 h-9 items-center justify-center">
-        <ButtonsGenericButton @click="subSyncTrigger"
-            class="bg-gray-0 group text-gray-400 bg-neutral-100 border-neutral-300 ring-neutral-400 flex w-9 h-9 items-center justify-center rounded-lg border fill-neutral-400 p-0 px-2 font-medium no-underline shadow-sm hover:bg-neutral-100 focus:ring-gray-100 focus:ring-offset-gray-100 dark:ring-neutral-500 focus:outline-none focus:ring-2 active:shadow-sm">
             <svg-icon name="subtitlesOn" class="h-[18px] fill-gray-400" v-if="props.activated" />
-            <svg-icon name="subtitlesOff" class="h-[18px] scale-110 fill-gray-400 group-hover:text-gray-500"
-                v-if="!props.activated" />
-        </ButtonsGenericButton>
+            <svg-icon name="subtitlesOff" class="h-[18px] scale-110 fill-gray-400 group-hover:text-gray-500" v-if="!props.activated" />
     </div>
 </template>
 
