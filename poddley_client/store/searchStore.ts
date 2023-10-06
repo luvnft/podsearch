88c5b87017207f523877e4ae2a432c5f4f9069ff
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-06 02:23:16 +0200

    Changing return types

--- poddley_client/store/searchStore.ts
+++ poddley_client/store/searchStore.ts
@@ -1,36 +1,36 @@
 import { defineStore } from "pinia";
-import { SearchResponse } from "types/SearchResponse";
+import { ClientSearchResponse } from "#build/types/ClientSearchResponse";
 import { SearchQuery } from "../types/SearchQuery";
 const utils: Utils = useUtils();
 
 export const useSearchStore = defineStore("searchStore", () => {
-  const searchQuery: Ref<SearchQuery> = ref({
-    searchString: "",
-    offset: 0,
-  });
+    const searchQuery: Ref<SearchQuery> = ref({
+        searchString: "",
+        offset: 0,
+    });
 
-  const previousSearchQuery: Ref<string> = ref("");
-  const loading: Ref<boolean> = ref(false);
-  const searchResults: Ref<SearchResponse> = ref({} as SearchResponse);
-  const setLoadingState = (loadingState: boolean) => (loading.value = loadingState);
-  const setSearchResults = (searchResponse: SearchResponse) => {
-    console.log("SearchResponse.query: ", searchResponse.query);
-    console.log("SearchQuery.value.searchstring: ", searchQuery.value.searchString);
-    if (searchResponse.query === previousSearchQuery.value) {
-      searchResults.value.hits = utils.removeDuplicates([...searchResults.value.hits, ...searchResponse.hits], "id");
-      console.log(searchResults.value.hits);
-    } else {
-      searchResults.value = searchResponse;
-    }
+    const previousSearchQuery: Ref<string> = ref("");
+    const loading: Ref<boolean> = ref(false);
+    const searchResults: Ref<ClientSearchResponse> = ref({} as ClientSearchResponse);
+    const setLoadingState = (loadingState: boolean) => (loading.value = loadingState);
+    const setSearchResults = (searchResponse: ClientSearchResponse) => {
+        console.log("SearchResponse.query: ", searchResponse.query);
+        console.log("SearchQuery.value.searchstring: ", searchQuery.value.searchString);
+        if (searchResponse.query === previousSearchQuery.value) {
+            searchResults.value.hits = utils.removeDuplicates([...searchResults.value.hits, ...searchResponse.hits], "id");
+            console.log(searchResults.value.hits);
+        } else {
+            searchResults.value = searchResponse;
+        }
 
-    previousSearchQuery.value = searchQuery.value.searchString || "";
-  };
-  //Returning
-  return {
-    searchQuery,
-    loading,
-    setLoadingState,
-    setSearchResults,
-    searchResults,
-  };
+        previousSearchQuery.value = searchQuery.value.searchString || "";
+    };
+    //Returning
+    return {
+        searchQuery,
+        loading,
+        setLoadingState,
+        setSearchResults,
+        searchResults,
+    };
 });
--- poddley_client/utils/services/TranscriptionsServiceSearch.ts
+++ poddley_client/utils/services/TranscriptionsServiceSearch.ts
@@ -1,29 +1,29 @@
 import axios, { AxiosResponse } from "axios";
 import { SearchQuery } from "../../types/SearchQuery";
-import { SearchResponse } from "../../types/SearchResponse";
+import { ClientSearchResponse } from "../../types/ClientSearchResponse";
 
 export default class TranscriptionsServiceSearch {
-  protected readonly BASE_URL = "https://api.poddley.com";
+    protected readonly BASE_URL = "https://api.poddley.com";
 
-  protected async fetchPost<T>(apiUrl: string, requestData: any): Promise<T> {
-    const response: AxiosResponse = await axios({
-      url: this.BASE_URL + apiUrl,
-      method: "POST",
-      data: requestData,
-    });
+    protected async fetchPost<T>(apiUrl: string, requestData: any): Promise<T> {
+        const response: AxiosResponse = await axios({
+            url: this.BASE_URL + apiUrl,
+            method: "POST",
+            data: requestData,
+        });
 
-    if (response.status >= 200 && response.status < 300) {
-      return response.data;
+        if (response.status >= 200 && response.status < 300) {
+            return response.data;
+        }
+
+        throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  public async search(searchQuery: SearchQuery): Promise<SearchResponse> {
-    console.log("Searching from api srrr");
-    console.log(searchQuery)
-    const data = this.fetchPost<any>("/transcriptions/search", { searchQuery });
-    console.log("DATA:", data);
-    return data;
-  }
+    public async search(searchQuery: SearchQuery): Promise<ClientSearchResponse> {
+        console.log("Searching from api srrr");
+        console.log(searchQuery)
+        const data = this.fetchPost<any>("/transcriptions/search", { searchQuery });
+        console.log("DATA:", data);
+        return data;
+    }
 }
