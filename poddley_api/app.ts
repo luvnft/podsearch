86e7b0cb7684a0bbb52beee25f9c528f10793fe8
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-07 23:06:40 +0000

    cleanup

--- poddley_api/app.ts
+++ poddley_api/app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import transcriptionsRouter from "./routes/transcriptions";
 import compression from "compression";
 
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = "production";
 
 //Setup
 const app: Express = express();
@@ -17,8 +17,8 @@ app.use(cors());
 //Enabling body
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
- 
-//Use the routes 
+
+//Use the routes
 app.use("/transcriptions", transcriptionsRouter);
 
 //Listen on port
