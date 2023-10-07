aea6e42c8051cb78462aa5bab46389f61688e3d7
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-07 16:54:13 +0200

    Fixes on frontend to support ned backend

--- poddley_client/components/SearchResults/SearchResults.vue
+++ poddley_client/components/SearchResults/SearchResults.vue
@@ -2,7 +2,7 @@
     <ul class="row mx-0 mb-14 mt-0 grid w-full grid-cols-1 justify-start gap-x-3 gap-y-3 p-0 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4"
         v-if="searchEntries.length > 0">
         <li class="m-0 p-0" v-for="(searchEntry, index) in props.searchEntries">
-            <SearchEntry :searchEntry="searchEntry" :index="index" />
+            <SearchEntry :searchEntry="searchEntry" :key="searchEntry.id" :index="index" />
         </li>
     </ul>
     <div class="m-0 flex h-5 w-full items-center justify-center p-0">
