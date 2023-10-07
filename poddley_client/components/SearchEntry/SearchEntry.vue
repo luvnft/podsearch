832ec586c511f1d0349483ea16e59b5f8f8a9282
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-08 00:42:26 +0200

    fix

--- poddley_client/components/SearchEntry/SearchEntry.vue
+++ poddley_client/components/SearchEntry/SearchEntry.vue
@@ -156,8 +156,8 @@ const handleTimeChange = async (event: Event) => {
 
                 // We loop over all the hits and create new segmentHits for the ones which have words bigger than some 5, essentially this
                 console.log("Index is: ", props.index)
-                // segmentHits = fragmentSegmentHits(segmentHits)
-                // searchResults.value.hits[props.index].subHits = segmentHits;
+                const segmentHits = fragmentSegmentHits(searchResponse.hits[0].subHits)
+                searchResults.value.hits[props.index].subHits = segmentHits;
                 console.log("SearchResults NOOOOOOW: ", searchResults.value)
                 console.log("Fragmentation done, should be set");
             }
