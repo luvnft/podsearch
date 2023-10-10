31f0e1dd882d6fb55371b765c9b907f623e42075
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-10 22:55:18 +0000

    yes

--- poddley_pm2_instances/app.config.js
+++ poddley_pm2_instances/app.config.js
@@ -7,18 +7,19 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         MEILI_HTTP_ADDR: "0.0.0.0:7700",
-        MEILI_MASTER_KEY: "MEILI_MASTER_KEY",
+        MEILI_MASTER_KEY: "",
         MEILI_DB_PATH: "/mnt/volume_nyc1_02/data.ms/",
       },
     },
     {
-      name: "Poddley API",
-      script: `/home/poddley/poddley_api/dist/app.js`,
-      watch: false,
+      name: "api",
+      script: "/home/poddley/poddley_api/dist/app.js",
+      watch: ["../../"], // Watches changes in the root folder
       env: {
         NODE_ENV: "production",
-        MEILI_MASTER_KEY: "MEILI_MASTER_KEY",
+        MEILI_MASTER_KEY: "",
       },
     },
-  ],
-};
+  ], 
+}; 
+ 
