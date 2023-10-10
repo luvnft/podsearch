623b4d8e041c1b150188167cb0e54e90f6b16a05
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-10 20:47:41 +0000

    backend stuff

--- poddley_api/other/meilisearchConnection.ts
+++ poddley_api/other/meilisearchConnection.ts
@@ -1,11 +1,8 @@
 import { MeiliSearch } from "meilisearch";
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config({ path: "../../.env" });
 
 const meilisearchConnection: MeiliSearch = new MeiliSearch({
-  host: process.env.MEILISEARCH_IP || "",
-  apiKey: "JZDCCGPEGFlfUZhbJ3s9JPtetws-6Xqpd38HbF2E1NU",
+  host: "https://meilisearch.poddley.com",
+  apiKey: process.env.MEILISEARCH_API_KEY || "",
 });
 
 export default meilisearchConnection;
- 
