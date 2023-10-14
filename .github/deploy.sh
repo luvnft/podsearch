3eec8fc0c78852ff3a73876036cab3b7698a2b72
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-14 23:31:19 +0000

    OK

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 # Set environment variables
-export PATH="$PATH:/root/.nvm/versions/node/v20.3.1/bin:/root/.nvm/versions/node/v20.3.1/bin/node:/root/.nvm/versions/node/v20.3.1/bin/pm2"
+export PATH="$PATH:/root/.nvm/versions/node/v20.3.1/bin:/root/.nvm/versions/node/v20.3.1/bin/node:/root/.nvm/versions/node/v20.3.1/bin/pm2:/root/.nvm"
 export PODDLEY_CLIENT_DIR="/home/poddley/poddley_client"
 
 echo "Hello!!!!"
-echo "PATH: $PATH"
-echo "Node.js Version: $(node --version)"
-echo "npm Version: $(npm --version)"
-which node
-which npm
+
 cd /home/poddley/poddley_client || exit 1  # Exit if the directory is not found
 git pull origin master
 npm install
