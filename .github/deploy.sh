d877264ec5341ad8aac450c7f0d07a0302f6741c
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-14 21:33:29 +0000

    ok

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# deploy.sh
-
-echo "Path: $PATH"
-echo "User: $(whoami)"
-which pm2
-
 cd /home/poddley/poddley_client || exit 1  # Exit if the directory is not found
-
 git pull origin master
 npm install
 npm run build
