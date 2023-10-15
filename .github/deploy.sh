9da999916c33ede6da5fdc2f1414b69df21d2ee8
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-15 00:05:27 +0000

    OK

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -3,9 +3,10 @@
 # Set environment variables
 export PATH="$PATH:/root/.nvm/versions/node/v20.3.1/bin:/root/.nvm/versions/node/v20.3.1/bin/node:/root/.nvm/versions/node/v20.3.1/bin/pm2:/root/.nvm"
 export PODDLEY_CLIENT_DIR="/home/poddley/poddley_client"
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh  # This loads NVM
 
 echo "Hello!!!!"
-/root/.nvm use 20
+nvm use 20
 cd /home/poddley/poddley_client || exit 1  # Exit if the directory is not found
 git pull origin master
 npm install
