ef007b4d59bf208c25f579f94b6472849220904d
Luka Momcilovic <luka_momcilovic@hotmail.com>
2023-10-15 03:39:23 +0200

    Ok

--- poddley_client/components/MainContainerSection/MainContainerSection.vue
+++ poddley_client/components/MainContainerSection/MainContainerSection.vue
@@ -1,7 +1,17 @@
 <template>
-    <main class="xs:px-0 xs:py-0 bg-white min-h-screen sm:py-1 px-0.5">
+    <main class="xs:px-0 xs:py-0 bg-white min-h-screen sm:py-1 px-0.5" v-if="mounted">
         <div class="container-fluid max-w-screen px-0.5 py-1">
             <slot />
         </div>
     </main>
 </template>
+
+<script setup lang = "ts">
+
+const mounted: Ref<boolean> = ref(false);
+
+onMounted(() => {
+    mounted.value = true;
+});
+
+</script>
